aa32fe7099b8d76573ad77151dc2d4bc46ba98d2
Wei Yang <wei@weizilla.com>
2018-04-20 23:04:29 -0500

    [Haskell] Add Grains

---
+++ haskell/grains/src/Grains.hs
@@ -0,0 +1,12 @@
+module Grains
+    ( square
+    , total
+    ) where
+
+square :: Integer -> Maybe Integer
+square n
+    | n <= 64 && n > 0 = Just $ 2 ^ (n - 1)
+    | otherwise = Nothing
+
+total :: Integer
+total = 2 ^ 64 - 1
